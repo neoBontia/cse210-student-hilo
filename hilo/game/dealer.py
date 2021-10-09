d6b06cf039d992712c182b2b05a64fe5d75aedb1
neoBontia <bon21004@byui.edu>
2021-10-09 22:29:46 +0800

    Fixed a bug in dealer.py

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -2,16 +2,23 @@ import random
 from game import Player
 
 class Dealer:
+    """
+    This class is the dealer or the director of the HiLo game.
+    This class is responsible for generating and displaying the cards
+    that the player will guess, and is also responsible for calculating
+    the points of the player based on his guess.
+    """
 
     def __init__(self):
-        self.card = 0
+        self.prev_card = random.randint(1, 13)
+        self.curr_card = 0
         self.points = 300
         self.player = Player()
 
     def display_card(self):
-        self.card = random.randint(1, 13)
+        self.curr_card = random.randint(1, 13)
 
-        print(f"The card is: {self.card}")
+        print(f"The card is: {self.prev_card}")
 
     def calculate(self):
         if self.player.update():
@@ -20,6 +27,14 @@ class Dealer:
         else:
             self.points -= 75
             print(f"Your score is: {self.points}")
+
+        self.prev_card = self.curr_card
+
+    def get_prev_card(self):
+        return self.prev_card
+
+    def get_curr_card(self):
+        return self.curr_card
 
     """def play(self):
         while self.player.can_play:
