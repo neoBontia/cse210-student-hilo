a42076263cae34210ac3697b8efe67c19e357d65
MasterBeans <jul21002@byui.edu>
2021-10-09 22:42:58 +0800

    added methods: guess_card, check_answer, update

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,3 +1,6 @@
+from game import Dealer
+
+
 class Player:
     """A code template for a player of HiLo game. The responsibility of this 
       class of objects is to guess if the next card is higher or lower than the current card, 
@@ -10,8 +13,19 @@ class Player:
 
     def __init__(self):
         self.guess = ""
+        self.is_correct = False
+        self.dealer = Dealer()
 
     def guess_card(self):
         self.guess = ""
-        self.guess = input()
+        self.guess = input("Higher or lower? [h/l] ")
         return self.guess
+
+    def check_answer(self):
+        self.is_correct = False
+        if (self.dealer.get_prev_card() < self.dealer.get_curr_card()):
+            if(self.guess == 'h' or self.guess == 'H'):
+                self.is_correct = True
+
+    def update(self):
+        return self.is_correct
